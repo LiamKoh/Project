00b4219bce8416f0881d8295fec3bfb2d3707400
anguswilliams91 <anguswilliams91@gmail.com>
2017-04-08 10:44:07 +0100

    script to add NTA code to taxi dataframe

---
+++ polygons.py
@@ -0,0 +1,30 @@
+from __future__ import division, print_function
+
+import pandas as pd 
+import matplotlib.path as mplPath
+import numpy as np 
+
+def get_nta_codes(lons_lats,geo):
+
+    nta_codes = np.empty(lons_lats.shape[0],dtype='object')
+    nta_codes[:] = ''
+
+    for nta_code in geo:
+
+        pts = geo[nta_code].dropna().values
+        pts = pts.reshape((pts.shape[0]/2,2))
+        nta_Path = mplPath.Path(pts)
+        is_in_nta = nta_Path.contains_points(lons_lats)
+        nta_codes[is_in_nta] = nta_code
+
+    return nta_codes
+
+def add_nta_codes(taxi_data,geo):
+
+    lons_lats = taxi_data.loc[:,['pickup_longitude','pickup_latitude']].as_matrix()
+    nta_codes = get_nta_codes(lons_lats,geo)
+    taxi_data.loc[:,'NTA_code'] = pd.Series(nta_codes,index=taxi_data.index)
+
+    return None
+
+
